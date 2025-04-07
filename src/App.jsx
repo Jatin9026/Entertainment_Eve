da9335459034a456a6f4eb5a012cc52a79336db4
TulikaZeth <tulikaanand05@gmail.com>
2025-04-07 19:20:46 +0530

    updarte

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ import LandingPage from "./Components/Hero_Section";
 import Past_Performers from "./Components/Past_Performers";
 import React from 'react';
 import TicketSection from "./Components/TicketSection";
+import Header from "./Components/NavBar";
 function App(){
   return <>
-  <NavBar/>
     <LandingPage/>
     <TicketSection/>
     <Past_Performers/>
