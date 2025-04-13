95b68fded5f7fb4996fb15fbd2c3493b6e9503c5
“BlackBlossom” <vikingpanther72@gmail.com>
2025-04-13 16:51:05 +0530

    Update

--- src/Components/Past_Performers.jsx
+++ src/Components/Past_Performers.jsx
@@ -60,7 +60,7 @@ const Past_Performers = () => {
           </h1>
           <h2 className="text-4xl md:text-5xl font-bold tracking-tight">
             EVENT{" "}
-            <span className=" bg-clip-text text-transparent custom-font">
+            <span className="bg-gradient-to-r from-teal-400 to-emerald-500 bg-clip-text text-transparent custom-font">
               UNSTOPPABLE
             </span>
           </h2>
